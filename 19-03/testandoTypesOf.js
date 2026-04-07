17d861261c990e986dff0920b8cf144ebbaf7c92
Mariana <marianarosseto10@gmail.com>
2026-04-07 19:07:41 -0300

    Add files via upload

---
+++ 19-03/testandoTypesOf.js
@@ -0,0 +1,20 @@
+/*const got ="game of thrones";
+const gotSeasens =8;
+
+
+console.log (typeof gotSeasens)
+
+console.log(typeof got )
+
+const iLoveGot = true;
+console.log ( typeof iLoveGot);*/
+
+
+
+
+
+const age =16
+const agetext = age.toString()
+
+console.log( typeof age,":" ,age)
+console.log( typeof agetext, " :" ,agetext)
