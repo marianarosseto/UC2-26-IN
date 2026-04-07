8b27e6e881e580195fa2e0a93b9fa476ff1237b2
Mariana <marianarosseto10@gmail.com>
2026-04-07 19:08:14 -0300

    Add files via upload

---
+++ 01-04/arrays.js
@@ -0,0 +1,32 @@
+/*const  ask = require("readline-sync")
+let name = ask.question("qual eh o seu nome:")
+console.log ("olá", name)
+*/
+
+/*let name = "mariana"
+let age =16
+let frase =`meu nome é ${name} e tenho ${age} anos`
+
+console.log( frase)*/
+
+
+/*const  ask = require("readline-sync")
+
+let name = ask.question("qual eh o seu nome: ")
+let cor = ask.question("qual sua cor favorita: ")
+let final = ( ` Meu nome eh ${name} e minha cor favorita eh ${cor}`)
+console.log (final)
+*/
+
+/*let nome = "Mariana"
+
+console.log (nome.length)*/
+
+/*let frase= (" hoje e fui à praia")
+ let fraseFiltrada = frase.includes ("praia")
+
+console.log (fraseFiltrada)*/
+
+ let frase = "Hoje eu comi cenoura, adoro cenoura"
+ let newFrase = frase.replaceAll ("cenoura", "batata")
+ console.log (newFrase)
