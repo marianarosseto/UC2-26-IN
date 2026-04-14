ad8f42555a1979527d83464a2fecc223d3985109
Mariana <marianarosseto10@gmail.com>
2026-04-14 19:12:59 -0300

    Add files via upload

---
+++ 08-04/arrays.js
@@ -0,0 +1,13 @@
+
+
+
+//let legumes= ["tomate","cebola"," chuchu"]
+//console.log(legumes[1])
+
+//let legumes= ["tomate","cebola"," chuchu"]
+//legumes [0] = "espinafre"
+//console.log(legumes)
+
+//let listaDeCompras= ["tomate","cebola"," chuchu"]
+//let  segundoItem = listaDeCompras[2]
+//console.log(segundoItem)
---
+++ 08-04/exercicio.js
@@ -0,0 +1,6 @@
+const ask = require("readline-sync")
+
+
+let raca =["pitibu","rotivale","pinxe","caramelo"," pudo"]
+let usuario = Number(ask.question (" ensira um numero de 0 a 4: "))
+console.log(raca[usuario])
