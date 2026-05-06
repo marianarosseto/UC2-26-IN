5f427a0301bed6bc7100b464b893ff2f8a5c56b1
Mariana <marianarosseto10@gmail.com>
2026-05-06 19:25:39 -0300

    Add files via upload

---
+++ 05-05/ex3-if-else.js
@@ -0,0 +1,12 @@
+ 
+    function comparar(num1, num2){
+        if(num1>num2){
+            return`${num1} é maior que ${num2} `
+
+     } else if (num1<num2){
+        return`${num1} é menor que ${num2} `
+        
+        } else
+            return`${num1} e ${num2} sao iguais`
+        }
+     
---
+++ 05-05/if.js
@@ -0,0 +1,44 @@
+/*let condição1 = true
+
+    if (condição1){
+        console.log("Entrei")
+    }
+
+
+    let condição2 = false
+    
+        if (condição2){
+            console.log("consicao")
+        }
+
+ 
+
+    function comparar(num1, num2){
+        if(num1=== num2){
+            return`${num1} e ${num2} sao iguais`
+     } else {
+        return `${num1} e ${num2} sao diferentes`
+     }
+        }
+        let firtsnumerber = 5
+        let secondnumber= 9
+    console.log(comparar(firtsnumerber, secondnumber))*/
+
+
+   
+    function comparar(num1, num2){
+        if(num1>num2){
+            return`${num1} é maior que ${num2} `
+
+     } else if (num1<num2){
+        return`${num1} é menor que ${num2} `
+        
+        } else
+            return`${num1} e ${num2} sao iguais`
+        }
+     
+        
+        let firtsnumerber = 8
+        let secondnumber= 8
+    console.log(comparar(firtsnumerber, secondnumber))
+        
